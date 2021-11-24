51f800456713f54774582cff791ad02c6283ab27
Steven Athouel <sathouel@gmail.com>
2021-11-24 12:25:47 +0200

    setup.py added

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="rakuten-api-client",
+    version="0.0.4",
+    author="Steven Athouel",
+    author_email="sathouel@gmail.com",
+    description="A simple api client for Rakuten plateform",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sathouel/rakuten_api_client",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
